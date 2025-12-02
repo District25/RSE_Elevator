aa1e6c051b12156557d2b9d1332eb810451bd778
Blatter Gregory <gregory.blatter@hes-so.ch>
2025-12-02 13:15:32 +0100

    Function initialize for initialized is completed

--- work/Elevator/src/security/maximumliftmonitor.cpp
+++ work/Elevator/src/security/maximumliftmonitor.cpp
@@ -1,2 +1,9 @@
 #include "maximumliftmonitor.h"
 
+namespace security{
+
+    void MaximumLiftMonitor::initialize(elevator::Controller & controller, motor::Decoder & decoder){
+        controller_ = &controller;
+        decoder_ = &decoder;
+    }   
+} // end of namespace
--- work/Elevator/src/security/maximumliftmonitor.h
+++ work/Elevator/src/security/maximumliftmonitor.h
@@ -1,7 +1,10 @@
 #ifndef SECURITY_MAXIMUM_LIFT_MONITOR_H
 #define SECURITY_MAXIMUM_LIFT_MONITOR_H
 
-#include "elevatorcontroller.h"
+#include "controller.h"
+#include "motor/decoder.h"
+#include "interface/elevatorcontrollerobserver.h"
+#pragma once
 
 namespace security {
 
@@ -9,11 +12,21 @@ namespace security {
     {
         public:
             MaximumLiftMonitor();
-            static void initialize();
-        protected:
-        
-    };
+            void initialize(elevator::Controller & controller, motor::Decoder & decoder);
+            /*elevator::Controller * get(){return controller_;}
+            void set(elevator::Controller* toto){controller_ = toto;}*/
 
-}
+        protected:
+            void onElevatorStarted() override;
+            void onElevatorReachedFloor(FloorNumber floorNumber) override;
+            void onElevatorError() override {}
+        private:
+            elevator::Controller * controller_ {nullptr};
+            motor::Decoder * decoder_ {nullptr};
+
+        protected:
+            const FloorNumber floorNumber;
+    };
+} // end of namespace
 
 #endif
